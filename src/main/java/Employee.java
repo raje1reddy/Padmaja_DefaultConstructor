e4a8cd5807291f569f612c5b1fface40e43b7fb3
unknown <raje1reddy@gmail.com>
2019-08-14 19:11:22 +0100

    code for default contructor

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,10 +2,6 @@
 class Employee {
     String name;
     int age;
-    Employee(){
-        age = 20;
-        System.out.println("user defined constructor");
 
-    }
 }
 
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class EmployeeTest {
     public static void main (String args[]) {
         Employee emp = new Employee();
-        System.out.println(emp.age);
+
     }
 
 }
